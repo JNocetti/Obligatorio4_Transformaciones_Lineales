80b373fee1c04430c785ab923bd5612d0689f7f0
IonasJospo <ionas.josponis@correo.ucu.edu.uy>
2023-12-08 20:14:15 -0300

    rotate with matrix

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 from scipy.ndimage import rotate
 from PIL import Image
@@ -26,10 +27,50 @@ def compress_image(image, k):
     return compressed_image
 
 
-def rotate_image(image, angle):
-    imageToArray = np.array(image)
-    rotatedImage = rotate(imageToArray, angle, reshape=False)
-    return rotatedImage
+def rotate_image(img, angle):
+    img = np.array(img)
+    #  convert rotation amount to radian
+    rotation_amount_rad = angle * np.pi / 180.0
+
+    #  get dimension info
+    height, width, num_channels = img.shape
+
+    #  create output image, for worst case size (45 degree)
+    max_len = int(math.sqrt(height*height + width*width))
+    rotated_image = np.zeros((max_len, max_len, num_channels))
+    #rotated_image = np.zeros((img.shape))
+
+    rotated_height, rotated_width, _ = rotated_image.shape
+    mid_row = int( (rotated_height+1)/2 )
+    mid_col = int( (rotated_width+1)/2 )
+
+    #  for each pixel in output image, find which pixel
+    #it corresponds to in the input image
+    for r in range(rotated_height):
+        for c in range(rotated_width):
+            #  apply rotation matrix, the other way
+            y = (r-mid_col)*math.cos(rotation_amount_rad) + (c-mid_row)*math.sin(rotation_amount_rad)
+            x = -(r-mid_col)*math.sin(rotation_amount_rad) + (c-mid_row)*math.cos(rotation_amount_rad)
+
+            #  add offset
+            y += mid_col
+            x += mid_row
+
+            #  get nearest index
+            #a better way is linear interpolation
+            x = round(x)
+            y = round(y)
+
+            #print(r, " ", c, " corresponds to-> " , y, " ", x)
+
+            #  check if x/y corresponds to a valid pixel in input image
+            if (x >= 0 and y >= 0 and x < width and y < height):
+                rotated_image[r][c][:] = img[y][x][:]
+
+
+    #  save output image
+    output_image = Image.fromarray(rotated_image.astype("uint8"))
+    return output_image
 
 
 def scaling(image, k):
